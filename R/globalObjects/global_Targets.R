d8c7fcf44641af92a09fcc388ce6c91b20dde378
dcarver1 <carverd@colostate.edu>
2023-03-16 12:15:49 -0600

    continue inputdata processing

--- R/globalObjects/global_Targets.R
+++ R/globalObjects/global_Targets.R
@@ -17,7 +17,31 @@ globalTargets <- list(
              format = "file"),
   ## process data 
   tar_target(name = biovars, command = processBioClim(rasters = rasterRDS,
-                                                      names = bioNames))
-  
+                                                      names = bioNames)),
+  # read in additional spatial data objects
+  # countries
+  tar_file_read(
+    countries, 
+    "data/geospatial_datasets/countries/ne_10m_admin_0_countries.gpkg",
+    read = sf::st_read(!!.x)
+  ),
+  #ecoregions
+  tar_file_read(
+    ecoregions, 
+    "data/geospatial_datasets/ecoregions/tnc_terr_ecoregions.gpkg",
+    read = sf::st_read(!!.x)
+  ),
+  # states 
+  tar_file_read(
+    states, 
+    "data/geospatial_datasets/states/ne_10m_admin_1_states_provinces.gpkg",
+    read = sf::st_read(!!.x)
+  ),
+  #counties
+  tar_file_read(
+    counties, 
+    "data/geospatial_datasets/counties/ne_10m_admin_2_counties.gpkg",
+    read = sf::st_read(!!.x)
+  )
 )
   
