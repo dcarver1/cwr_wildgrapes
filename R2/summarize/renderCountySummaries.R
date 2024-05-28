809dee476d1b3d7447de1b5d0973e43812c3931c
dcarver1 <carver.dan1@gmail.com>
2024-05-28 16:41:26 -0700

    sytle updates to county maps and link in eval popup

--- R2/summarize/renderCountySummaries.R
+++ R2/summarize/renderCountySummaries.R
@@ -118,7 +118,7 @@ generateOccurnaceRMD <- function(species1){
     )
 }
 # # ## needs to be commented out unless running 
-# fullSpecies |> purrr::map(generateOccurnaceRMD)
+fullSpecies |> purrr::map(generateOccurnaceRMD)
 # speciesList |> purrr::map(generateOccurnaceRMD)
 # fullSpeciesTrim[1:length(fullSpeciesTrim)] |> purrr::map(generateOccurnaceRMD)
 # erroredSpecies |> purrr::map(generateOccurnaceRMD)
