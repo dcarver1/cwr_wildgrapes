d82f492f58f3e905e181e0c59e888ccc76e08b72
dcarver1 <carver.dan1@gmail.com>
2024-07-22 11:53:08 -0700

    add PCA to boxplots

--- R2/summarize/renderCountySummaries.R
+++ R2/summarize/renderCountySummaries.R
@@ -120,12 +120,12 @@ generateOccurnaceRMD <- function(species1){
     )
 }
 # # ## needs to be commented out unless running 
-fullSpecies |> purrr::map(generateOccurnaceRMD)
+# fullSpecies |> purrr::map(generateOccurnaceRMD)
 # speciesList |> purrr::map(generateOccurnaceRMD)
 # fullSpeciesTrim[1:length(fullSpeciesTrim)] |> purrr::map(generateOccurnaceRMD)
 # erroredSpecies |> purrr::map(generateOccurnaceRMD)
 # ### troubleshooting
-generateOccurnaceRMD(species ="Vitis baileyana")
+# generateOccurnaceRMD(species ="Vitis baileyana")
 
 ## erroring out at specific species need to troubleshoot that directly 
 ## "Vitis tiliifolia"
