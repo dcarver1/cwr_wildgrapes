89b36812d168b8e5ea70680f3aeb215e714fce8b
dune <carver.dan1@gmail.com>
2025-12-08 19:48:08 +0000

    update the preprocess for simple vect wdpa features

--- preprocessing/produceProtectAreas.R
+++ preprocessing/produceProtectAreas.R
@@ -8,18 +8,38 @@
 
 
 
-pacman::p_load("terra", "tmap", "dplyr", "tictoc", "tidyterra")
+pacman::p_load("terra", "tmap", "dplyr", "tictoc", "tidyterra","rmapshaper")
 
-bioVars <- readRDS("data/geospatial_datasets/bioclim_layers/bioclim_2.5arcsec_terra.RDS")%>%
-  unwrap()
+# bioVars <- readRDS("data/geospatial_datasets/bioclim_layers/bioclim_2.5arcsec_terra.RDS")%>%
+#   unwrap()
 
-temp1 <- bioVars[[1]]
-
-rm(bioVars)
+# temp1 <- bioVars[[1]]
+# 
+# rm(bioVars)
 
 
 f1 <- list.files(path = "data/geospatial_datasets/protectedLands/WDPA_Mar2023_Public_shp", pattern = ".gpkg",
            recursive = TRUE, full.names = TRUE)
+for(file in seq_along(f1)){
+  export <- paste0("data/geospatial_datasets/protectedLands/wdpa_",file,".gpkg")
+  if(!file.exists(export)){
+    p1 <- vect(f1[file])
+    # remove all marine areas before dim(91055,30) -- after dim(89259    30)p1 <- p1[p1$MARINE %in% c("0","1"),] 
+    p2 <- p1 %>% 
+      tidyterra::filter(MARINE %in% c("0","1")) |>#0 = terrestrial, 1 = Coastal 
+      terra::makeValid()
+    # simplfy 
+    p3 <- terra::simplifyGeom(p2, tolerance = 0.001, makeValid = TRUE)
+    # export 
+    terra::writeVector(x = p3, 
+                       filename = export,
+                       overwrite = TRUE)
+    rm(p2)
+    rm(p3)
+    gc()
+  }
+}
+
 
 for(file in seq_along(f1)){
   p1 <- vect(f1[file])
